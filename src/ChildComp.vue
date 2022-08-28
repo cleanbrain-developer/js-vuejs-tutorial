fa31bc4d4fc744620b9ee37d44b71d6a0dba05e6
cleanbrain-developer <cleanbrain-developer@gmail.com>
2022-08-28 17:43:52 +0900

    2022-08-28 : commit

---
+++ src/ChildComp.vue
@@ -0,0 +1,10 @@
+<script setup>
+// eslint-disable-next-line no-undef
+const emit = defineEmits(['response'])
+
+emit('response', 'hello from child')
+</script>
+
+<template>
+  <h2>Child component</h2>
+</template>
