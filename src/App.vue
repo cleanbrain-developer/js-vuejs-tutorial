77de313c52ada8bba07547557366d58ea0fe83bf
cleanbrain-developer <cleanbrain-developer@gmail.com>
2022-08-28 17:14:49 +0900

    2022-08-28 : commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const p = ref(null)
+
+onMounted(() => {
+  p.value.textContent = 'mounted!'
+})
+
+</script>
+
+<template>
+  <p ref="p">hello</p>
+</template>
